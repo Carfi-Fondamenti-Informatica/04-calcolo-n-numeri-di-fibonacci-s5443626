dafc7b1424386043643ab259baecc28df8e13846
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-11-22 16:19:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+    int primo_numero=0;
+    int secondo_numero=1;
+    int numero;
+    cin >> numero;
+    int i=3;
+    if (numero > 2){
+       int risultato;
+   while (i++  <= numero){
+       risultato = primo_numero + secondo_numero;
+       cout << risultato << " ";
+       primo_numero = secondo_numero;
+       secondo_numero = risultato;
+   }else {cout << "errore};
    return 0;
 }
